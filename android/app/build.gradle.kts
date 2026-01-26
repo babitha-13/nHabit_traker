063a697c6f51bf4c699e321c10911473a69041b6
Nikhil <math.nik@gmail.com>
2026-01-26 20:00:30 +0530

    morning catch up logic fix, repetition removed

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
         applicationId = "com.example.untitled"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion  // Required by firebase_auth plugin
+        minSdk = 23  // Required by firebase_auth plugin (minimum SDK 23)
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
